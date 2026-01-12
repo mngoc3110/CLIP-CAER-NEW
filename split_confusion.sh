c4d9d18b98bc6cbaff225195985b56be8c60f3b9
mngochocsupham <darber3110@gmail.com>
2026-01-12 22:05:24 +0700

    chỉnh sửa mi và dc

--- split_confusion.sh
+++ split_confusion.sh
@@ -1,17 +1,23 @@
+#!/bin/bash
+set -e
+
+RESUME_CKPT="/kaggle/input/clip-caer-new-v1/CLIP-CAER-NEW-V1/model_best.pth"
+
 python main.py \
   --mode train \
-  --resume-from "/kaggle/input/clip-caer-new-v1/CLIP-CAER-NEW-V1/model_best.pth" \
+  --resume-from "${RESUME_CKPT}" \
   --exper-name stage2_conf_split_softgate \
   --gpu 0 \
-  --epochs 50 \
+  --epochs 30 \
   --batch-size 8 \
   --optimizer AdamW \
   --lr 5e-5 \
   --lr-image-encoder 1e-6 \
   --lr-prompt-learner 2e-4 \
   --lr-adapter 5e-5 \
-  --weight-decay 0.0001 \
-  --milestones 10 13 \
+  --weight-decay 1e-4 \
+  \
+  --milestones 20 23 \
   --gamma 0.1 \
   \
   --two-head-loss \
@@ -19,13 +25,13 @@ python main.py \
   --w-4 1.0 \
   --soft-gate-thr 0.7 \
   \
-  --root-dir /kaggle/input/raer-video-emotion-dataset \
-  --train-annotation /kaggle/input/raer-annot/annotation/train.txt \
-  --val-annotation /kaggle/input/raer-annot/annotation/test.txt \
-  --test-annotation /kaggle/input/raer-annot/annotation/test.txt \
-  --clip-path ViT-B/32 \
-  --bounding-box-face /kaggle/input/raer-video-emotion-dataset/RAER/bounding_box/face.json \
-  --bounding-box-body /kaggle/input/raer-video-emotion-dataset/RAER/bounding_box/body.json \
+  --lambda-mi 0.03 \
+  --mi-warmup 16 \
+  --mi-ramp 3 \
+  \
+  --lambda-dc 0.05 \
+  --dc-warmup 16 \
+  --dc-ramp 3 \
   \
   --text-type prompt_ensemble \
   --contexts-number 12 \
@@ -37,14 +43,15 @@ python main.py \
   --duration 1 \
   --image-size 224 \
   \
-  --lambda_mi 0.05 \
-  --mi-warmup 0 \
-  --mi-ramp 5 \
-  --lambda_dc 0.08 \
-  --dc-warmup 0 \
-  --dc-ramp 5 \
-  \
   --use-amp \
   --grad-clip 1.0 \
   --seed 42 \
-  --print-freq 10
+  --print-freq 10 \
+  \
+  --root-dir /kaggle/input/raer-video-emotion-dataset \
+  --train-annotation /kaggle/input/raer-annot/annotation/train.txt \
+  --val-annotation /kaggle/input/raer-annot/annotation/test.txt \
+  --test-annotation /kaggle/input/raer-annot/annotation/test.txt \
+  --clip-path ViT-B/32 \
+  --bounding-box-face /kaggle/input/raer-video-emotion-dataset/RAER/bounding_box/face.json \
+  --bounding-box-body /kaggle/input/raer-video-emotion-dataset/RAER/bounding_box/body.json
